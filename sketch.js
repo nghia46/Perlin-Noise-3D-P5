9b330593794513df670d8eaaa3065e5f3193624d
nghia46 <nghialoe46@gmail.com>
2024-02-19 13:26:58 +0700

    update the visualize

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-const cols = 100;
-const rows = 100;
+const cols = 140;
+const rows = 140;
 
 let angleX = 0;
 let angleY = 0;
 
 function setup() {
-  frameRate(144);
+  createCanvas(700, 700, WEBGL);
 
-  createCanvas(500, 500, WEBGL);
+  zoomLeverInput = document.getElementById('zoomLevel');
 }
 
 function draw() {
@@ -20,8 +20,11 @@ function draw() {
   rotateX(angleX);
   rotateY(angleY);
 
+  scale(parseFloat(zoomLeverInput.value));
+
   translate(-width / 2, -height / 2);
 
+  noStroke();
   for (let y = 0; y < rows; y++) {
     beginShape(TRIANGLE_STRIP);
     for (let x = 0; x < cols; x++) {
@@ -46,17 +49,39 @@ function draw() {
 }
 
 function getTerrainColor(h) {
-  if (h < -70) {
-    // Ocean
-    return color(0, 0, 255);
-  } else if (h < -40) {
+  if (h < -40) {
+    //Deep Ocean
+    return color(75, 91, 171);
+  } 
+  else if (h < -30) 
+  {
+    //
+    return color(77, 166, 255);
+
+  } 
+  else if (h < -10) 
+  {
+    //
+    return color(120, 255, 227);
+  } 
+  else if (h < 0) 
+  {
     // Beach
     return color(230, 210, 170);
-  } else if (h < 20) {
+  } 
+  else if (h < 40) 
+  {
     // Grass
-    return color(50, 205, 50);
-  } else {
+    return color(124, 184, 57);
+  } 
+  else if(h < 50)
+  {
     // Mountain
     return color(100, 100, 100);
+  }
+  else 
+  {
+    //Snow
+    return color(255, 255, 237);
   }
 }
